e085ae7f484702129d5a41fb81b7c77d68ca497a
takeyoshinitta <c6afc0c0b9d6bc@gmail.com>
2023-03-16 07:15:31 +0800

    use google chart library

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/process_file', methods=['POST'])
+def process_file():
+    # get the file from the request
+    file = request.files['file']
+    # read the contents of the file
+    contents = file.read()
+    # convert the contents to a string
+    contents_str = contents.decode('utf-8')
+
+    return render_template('index.html', string=contents_str)
+
+if __name__ == '__main__':
     app.run(debug=True)
