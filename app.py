a8409382865c6348d0de8c781dc4f7e887da0bff
takeyoshinitta <c6afc0c0b9d6bc@gmail.com>
2023-03-16 23:14:55 -1000

    deploy setting

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    file_path = url_for('static', filename='image/wordcloud.png')
+
+    # Remove existing word cloud image
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        
     return render_template('index.html')
 
 @app.route('/process_file', methods=['POST'])
